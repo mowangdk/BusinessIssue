fa07ab4f430b73930d131022fbefc2cf5f1360a8
Sui Chen <chensuim@126.com>
2019-03-19 23:09:35 +0800

    added astar

--- tsp.py
+++ tsp.py
@@ -75,116 +75,178 @@ def check(permu):
     return max(count.values()) < 2
 
 
-def brute_force(points, distances, d):
-    n = len(points)
-    g = itertools.permutations(range(n))
+def perm(a, k=0):
+    if k == len(a):
+        yield a
+    else:
+        for i in xrange(k, len(a)):
+            if a[i] != a[k]:
+                a[k], a[i] = a[i], a[k]
+                g = perm(a, k+1)
+                for x in g:
+                    yield x
+                a[k], a[i] = a[i], a[k]
+
+
+def brute_force(points, distances, dd):
+    points = map(tuple, points)
+    d = dict()
+    for point in points:
+        if point not in d:
+            d[point] = chr(97+len(d))
+    rd = dict([(v,k) for k, v in d.items()])
+    indexd = dict()
+    for i, point in enumerate(points):
+        indexd[point] = i
+    g = collections.defaultdict(dict)
+    for p1 in d:
+        i1 = indexd[p1]
+        for p2 in d:
+            i2 = indexd[p2]
+            g[d[p1]][d[p2]] = distances[i1][i2]
+    p = [d[point] for point in points[1:]]
+    permutations = itertools.permutations(p)
     count = 0
     best = float('inf')
     route = None
-    for permu in g:
-        dis = calculate(permu, distances)
-        poss = check(permu)
-        if poss and dis != float('inf'):
-            count += 1
-            if dis < best:
-                best = dis
-                route = permu
-    if route is None:
-        raise ValueError()
-    route = [points[i] for i in route]
-    route.append(route[0])
-    return best*d, route, count
+    start = d[points[0]]
+    for permu in permutations:
+        s = start + ''.join(permu) + start
+        c = set()
+        reused = False
+        for i in range(len(s)-1):
+            v = s[i:i+2]
+            if v in c:
+                reused = True
+                break
+            else:
+                c.add(v)
+                c.add(v[::-1])
+        if not reused:
+            dis = 0
+            for i in range(len(s)-1):
+                dis += g[s[i]][s[i+1]]
+            if dis != float('inf'):
+                count += 1
+                if dis < best:
+                    best = dis
+                    route = s
+    if route is not None:
+        route = [rd[char] for char in route]
+    return best*dd, route, count / 2
+
+
+def dfs(points, distances, d):
+    points = map(tuple, points)
+    d = dict()
+    for point in points:
+        d[point] = chr(97+len(d))
+    rd = dict([(v,k) for k, v in d.items()])
+    indexd = dict()
+    for i, point in enumerate(points):
+        indexd[point] = i
+    g = collections.defaultdict(dict)
+    for p1 in d:
+        i1 = indexd[p1]
+        for p2 in d:
+            if p1 != p2:
+                i2 = indexd[p2]
+                if distances[i1][i2] != float('inf'):
+                    g[d[p1]][d[p2]] = distances[i1][i2]
+    memo = dict()
+    mmax = [0] * len(d)
+    for point in points:
+        mmax[ord(d[point])-97] += 1
+
+    def helper(s):
+        count = [0] * len(d)
+        for char in s:
+            count[ord(char)-97] += 1
+        if count > mmax:
+            return float('inf'), 0
+        if len(s) == len(points):
+            return g[s[-1]][s[0]], 1
+        if s not in memo:
+            l, c = float('inf'), 0
+            for other in g[s[-1]]:
+                v1 = s[-1] + other
+                v2 = other + s[-1]
+                if v1 not in s and v2 not in s:
+                    ol, oc = helper(s+other)
+                    c += oc
+                    l = min(l, ol + g[s[-1]][other])
+            memo[s] = (l, c)
+        return memo[s]
+    return helper('a')
 
 
 def build_vec(p1, p2):
     if p1 > p2:
         p1, p2 = p2, p1
-    return tuple(p1+p2)
+    return p1+p2
 
 
-def Astar(points, distances, d):
-    n = len(points)
+def Astar(points, distances, dd):
+    points = map(tuple, points)
+    d = dict()
+    for point in points:
+        if point not in d:
+            d[point] = chr(97+len(d))
+    rd = dict([(v,k) for k, v in d.items()])
+    indexd = dict()
+    for i, point in enumerate(points):
+        indexd[point] = i
+    g = collections.defaultdict(dict)
+    for p1 in d:
+        i1 = indexd[p1]
+        for p2 in d:
+            i2 = indexd[p2]
+            if distances[i1][i2] != float('inf'):
+                g[d[p1]][d[p2]] = distances[i1][i2]
     q = list()
-    g = [[] for _ in range(n)]
-    for i in range(n):
-        for j in range(i):
-            if distances[i][j] != float('inf'):
-                g[i].append(j)
-                g[j].append(i)
-    q.append((0, [0], ()))
+    q.append((0, 'a'))
     count = 0
     best = float('inf')
     route = None
+    num = collections.Counter()
+    for point in points:
+        num[d[point]] += 1
     while q:
-        dis, path, vecs = q.pop()
+        dis, path = q.pop()
         if len(path) == len(points):
             head = path[0]
             tail = path[-1]
-            vec = build_vec(points[head], points[tail])
-            if vec not in vecs:
+            v1 = head + tail
+            v2 = tail + head
+            if v1 not in path and v2 not in path and tail in g[head]:
                 count += 1
-                if dis + distances[head][tail] < best:
-                    route = [points[i] for i in path]
-                    route.append(route[0])
-                    best = dis + distances[head][tail]
+                if dis + g[head][tail] < best:
+                    route = path
+                    best = dis + g[head][tail]
         else:
             for other in g[path[-1]]:
-                vec = build_vec(points[other], points[path[-1]])
-                if other not in path and vec not in vecs:
-                    q.append((dis + distances[path[-1]][other], path + [other], vecs + (vec,)))
-    if route is None:
-        raise ValueError()
-    return best*d, route, count / 2
+                v1 = path[-1] + other
+                v2 = other + path[-1]
+                if v1 not in path and v2 not in path and path.count(other) < num[other]:
+                    q.append((dis + g[path[-1]][other], path + other))
+    if route is not None:
+        route = [rd[i] for i in route]
+        route.append(route[0])
+    return best*dd, route, count / 2
 
 
 def tsp_dp(points, d, restriction):
-    t = time.time()
     distances = get_distances(points, restriction)
-    print 'dis calculate %s' % (time.time() - t)
-    t = time.time()
     ret = Astar(points, distances, d)
-    print 'astar %s' % (time.time() - t)
-    t = time.time()
-    #retb = brute_force(points, distances, d)
-    retb = None
-    print 'brute force %s' % (time.time() - t)
-    return ret, retb
+    return ret
 
 
 def tsp_dp_with_path(points, d, path):
     distances = get_distances_with_path(points, path)
-    return brute_force(points, distances, d)
-
-
-def tsp_greedy(points, d):
-    distances = get_distances(points)
-    m = len(points)
-    total = set(range(m))
-    minimum = float('inf')
-    res_route = None
-    for start in range(m):
-        final_route = [start]
-        walked = 0
-        rest = set(total)
-        rest.remove(start)
-        while len(final_route) < m:
-            this = final_route[-1]
-            nearest = float('inf')
-            next_city = None
-            for other in rest:
-                if distances[this][other] < nearest:
-                    next_city = other
-                    nearest = distances[this][other]
-            rest.remove(next_city)
-            final_route.append(next_city)
-            walked += nearest
-        walked += distances[final_route[-1]][start]
-        final_route.append(start)
-        if walked < minimum:
-            minimum = walked
-            res_route = final_route
-    return minimum * d, map(lambda x: points[x], res_route)
+    return Astar(points, distances, d)
 
 
 if __name__ == '__main__':
-    print tsp_dp([[0,0],[0,1],[1,0],[1,1],[2,0],[2,1],[3,0],[3,1],[4,0],[4,1],[0,0]], 1, [])
+    a =[[0,0],[0,1],[1,0],[1,1],[2,0],[2,1],[3,0],[3,1],[4,0],[1,1],[0,0]]
+    b = [[0,0],[0,1],[1,0]]
+    print tsp_dp([[0,0],[0,1],[1,0],[1,1]], 1, [])
